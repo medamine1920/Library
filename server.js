44b10e53ecbb147c16671ca97e8d4cb473c5ea67
medamine1920 <med618890@gmail.com>
2022-11-01 21:44:29 +0100

    0.1

--- server.js
+++ server.js
@@ -21,6 +21,9 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, URL, {useNewUrlParser: true })
 const db = mongoose.connection
 
+
+const url = "mongodb+srv://medamine:medamine@clustername.mongodb.net/test?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true";
+
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected !'))
 
